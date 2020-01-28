13b55f1696668fccb19fad07cf3cf72909313a55
Hamid Ashhad <bitf16a032@pucit.edu.pk>
2020-01-28 19:20:14 +0500

    final commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            var currDateTime = DateTime.Now;
+            ViewBag.Time = currDateTime;
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if(ModelState.IsValid)
+                return View("Thanks", guestResponse);
+            return View();
+        }
+
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Please Enter you Email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please Enter a valid Email")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Please Enter your Phone")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please Specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
